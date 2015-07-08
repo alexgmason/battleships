5df188642868722452d8a855b7174e37c5fddc56
Alex Mason <alexgmason@gmail.com>
2015-07-08 15:49:10 +0100

    seperate out spec files

---
+++ spec/board-spec.js
@@ -0,0 +1,110 @@
+var Board = require('../src/board.js');
+
+describe('A one sided game of Battleships board that is responsible for creating/positioning ships and keeping track of their positions on the board', function(){
+  var coordinates;
+  var board;
+
+  var GRIDSIZE = 10;
+  var SHIPSIZE = 5;
+  var shipBlueprint = [
+    { type: 'Battleship',
+      size: SHIPSIZE,
+    }
+  ];
+
+  beforeEach(function(){
+    coordinates = false;
+    board = new Board(GRIDSIZE, shipBlueprint).init();
+  });
+
+  it('Given a ship blueprint, should add instantiate the ship and add it to the ships array', function(){
+    expect(board.ships.length).toEqual(1);
+    board.addNewShip(shipBlueprint);
+    expect(board.ships.length).toEqual(2);
+  });
+
+  it('Should position a given ship. By returning an array of coordinates of passed in size', function(){
+    while(!coordinates){
+      coordinates = board.attemptToPositionShip(SHIPSIZE);
+    }
+    expect(coordinates.length).toEqual(SHIPSIZE);
+  });
+
+
+  it('Given an empty board and a shot at a specific location, that same position should return the correct shot data', function(){
+    var shotCoordinates = [3, 4];
+    var expectedHitData = {
+      coordinates: [3 ,4],
+      hit: false
+    };
+
+    board.recordShot(shotCoordinates);
+
+    expect(board.getPreviousShotDataForPosition(shotCoordinates)).toEqual(expectedHitData);
+  });
+
+  it('Given a ship placed on the board and a shot recorded at that position, given that same position should return the correct shot data', function(){
+    shotCoordinates = board.ships[0].coordinates[0];
+    var expectedHitData = {
+      coordinates: shotCoordinates,
+      hit: true
+    };
+
+    board.recordShot(shotCoordinates, board.ships[0]);
+
+    expect(board.getPreviousShotDataForPosition(shotCoordinates)).toEqual(expectedHitData);
+  });
+
+  it('Given an empty board. A check to see if a ship is at a given location should be false', function(){
+    board.init();
+
+    var coordinateToCheck = [0,5];
+    var shipIsAtPosition = board.checkIfShipIsAtPosition(coordinateToCheck);
+    expect(shipIsAtPosition).toEqual(false);
+  });
+
+  it('Given a ship positioned on the board, then given a coordinate at that ships location. A check to see if a ship is at that location should be true', function(){
+    board.init();
+
+    while(!coordinates){
+      coordinates = board.attemptToPositionShip(SHIPSIZE);
+    }
+
+    board.ships[0] = {
+      coordinates: coordinates
+    };
+
+    var coordinateToCheck = coordinates[0];
+    var shipIsAtPosition = board.checkIfShipIsAtPosition(coordinateToCheck);
+    expect(shipIsAtPosition).toEqual(true);
+  });
+
+  it('Returns random orientation in a valid format', function(){
+    var orientation = board.getRandomOrientation();
+
+    var orientationIsValid = orientation === 'horizontal' || orientation === 'vertical';
+    expect(orientationIsValid).toBeTruthy();
+  });
+
+  it('Returns random coordinates in a valid format', function(){
+    var coordinates = board.getRandomLocation();
+
+    expect(coordinates.length).toBe(2);
+
+    var firstCoordinateIsLessThanEqualToGridSize = coordinates[0] <= GRIDSIZE;
+    var secondCoordinateIsLessThanEqualToGridSize = coordinates[1] <= GRIDSIZE;
+
+    expect(firstCoordinateIsLessThanEqualToGridSize).toBeTruthy();
+    expect(secondCoordinateIsLessThanEqualToGridSize).toBeTruthy();
+  });
+
+});
+//Grid input out of bounds should be reported
+
+//A cell that has been targeted cannot be targeted again
+
+//A cell with no ship returns 'no hit'
+//A cell with a ship returns 'hit' & hit is recorded
+
+//A Ships last remaining cell sinks the battleship and removes it from the board
+//A board with all ships sank completes the game
--- spec/game-spec.js
+++ spec/game-spec.js
@@ -1,21 +1,41 @@
 var Game = require('../src/game.js');
-var Board = require('../src/board.js');
-var Ship = require('../src/ship.js');
-
-var GRIDSIZE = 10;
-var SHIPS = [
-  {
-    'type': 'Battleship',
-    'size': 5
-  }
-];
 
 //Grid size should be 10x10
 describe('A one sided game of Battleships game object', function(){
   var game;
+
+  var GRIDSIZE = 10;
+  var SHIPS = [
+    {
+      'type': 'Battleship',
+      'size': 5
+    }
+  ];
+
   beforeEach(function(){
     game = new Game(GRIDSIZE, SHIPS);
     game.init();
+  });
+
+  afterEach(function() {
+    game = undefined;
+  });
+
+  it('Should convert an alphanumeric coordinates into an array of integers indexed zero', function(){
+    var coordinates = 'C1';
+    var convertedCoordinates = game.convertCoordinatesToIndex(coordinates);
+
+    expect(convertedCoordinates).toEqual([2,0]);
+  });
+
+  it('Should return an array of alphabetised letters of the length of grid size', function(){
+    var expectedArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g' ,'h', 'i', 'j'];
+    expect(game.getValidXAxisLetters()).toEqual(expectedArray);
+  });
+
+  it('Should return a regex that matchtes valid letters', function(){
+    var expectedRegex = /[a-j]/i;
+    expect(game.getCoordinatesLetterRegex()).toEqual(expectedRegex);
   });
 
   it('Should accept valid grid references, with one letter and one number', function(){
@@ -58,57 +78,13 @@ describe('A one sided game of Battleships game object', function(){
     expect(game.checkInputtedCoordinatesAreValid(GRIDREFERENCE)).toBe(false);
   });
 
-  it('Should convert an alphanumeric coordinates into an array of integers indexed zero', function(){
-    var coordinates = 'C1';
-    var convertedCoordinates = game.convertCoordinatesToIndex(coordinates);
-
-    expect(convertedCoordinates).toEqual([2,0]);
+  it('Should continue the game when every ship has NOT been sunk', function(){
+    expect(game.checkForEndOfGame()).toEqual(false);
   });
+
+  it('Should end the game when every ship has been sunk', function(){
+    game.board.ships[0].sunk = true;
+    expect(game.checkForEndOfGame()).toEqual(true);
+  });
+
 });
-describe('A one sided game of Battleships board that is responsible for creating/positioning ships and keeping track of their positions on the board', function(){
-  var coordinates;
-  var board;
-
-  var SHIPSIZE = 5;
-  var shipBlueprint = {
-    type: 'Battleship',
-    size: SHIPSIZE,
-  };
-
-  beforeEach(function(){
-    coordinates = false;
-    board = new Board(GRIDSIZE, shipBlueprint).init();
-  });
-
-  it('Should position a given ship. By returning an array of coordinates of passed in size', function(){
-    while(!coordinates){
-      coordinates = board.attemptToPositionShip(SHIPSIZE);
-    }
-    expect(coordinates.length).toEqual(SHIPSIZE);
-  });
-
-  it('Given a ship on the board, given a coordinate at that ships location. A check to see if a ship at that location should be true', function(){
-    board.init();
-
-    while(!coordinates){
-      coordinates = board.attemptToPositionShip(SHIPSIZE);
-    }
-
-    board.ships[0] = {
-      coordinates: coordinates
-    };
-
-    var coordinateToCheck = coordinates[0];
-    var shipIsAtPosition = board.checkIfShipIsAtPosition(coordinateToCheck);
-    expect(shipIsAtPosition).toEqual(true);
-  });
-});
-//Grid input out of bounds should be reported
-
-//A cell that has been targeted cannot be targeted again
-
-//A cell with no ship returns 'no hit'
-//A cell with a ship returns 'hit' & hit is recorded
-
-//A Ships last remaining cell sinks the battleship and removes it from the board
-//A board with all ships sank completes the game
