9d3f0767c7437196bfbede7cde42d483a5adcb1a
Alex Mason <alexgmason@gmail.com>
2015-07-08 18:26:22 +0100

    Add ship tests

---
+++ spec/ship-spec.js
@@ -0,0 +1,34 @@
+var Ship = require('../src/ship.js');
+
+//Grid size should be 10x10
+describe('A ship', function(){
+  var ship;
+  var SHIPSIZE = 5;
+
+  var shipData = {
+    type: 'Battleship',
+    size: SHIPSIZE
+  };
+
+  beforeEach(function(){
+    ship = new Ship(shipData).init();
+  });
+
+  it('should increase it\'s hit count by one each time it is hit', function(){
+    ship.recordDamage();
+    expect(ship.hitCount).toEqual(1);
+  });
+
+  it('should set sunk to true when the ship is sunk', function(){
+    ship.sinkShip();
+    expect(ship.sunk).toBeTruthy();
+  });
+  it('should sink when the hitcounter is equal to the size of the ship', function(){
+    for(var i = 0; i < ship.size; i++){
+      ship.recordDamage();
+    }
+    expect(ship.sunk).toBeTruthy();
+  });
+
+
+});
